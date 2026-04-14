9872372e19b878323328634b14661723d6191f7d
Dkrenon <bgsbonilla@gmail.com>
2026-04-14 13:12:28 +0100

    chore: adjust home logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,23 @@
 	import logoUrl from '../../icons/logo.svg';
 </script>
 
-<section class="flex flex-col justify-center md:flex-row items-center min-h-[80vh] px-4">
-	<div class="flex flex-col items-center">
-		<img src={logoUrl} alt="O'RUGGIN logo" class="logo" />
-	</div>
-	<div class="flex flex-col">
+<section class="flex min-h-[80vh] flex-col items-center justify-center px-4">
+	<div class="flex flex-col items-end justify-center gap-2">
 		<h1 class="font-bold">O'Ruggin Trail</h1>
 		<h2 class="bg-black text-right font-bold text-white">PRESSKIT</h2>
 		<em>Every Hero needs a Story</em>
 	</div>
+	<div class="flex flex-col items-center">
+		<img src={logoUrl} alt="O'RUGGIN logo" class="logo" />
+	</div>
 </section>
 
 <style>
-	:global(:root) {
-		--background-page: var(--col-home);
-	}
-	.logo {
-		width: 15rem;
-		height: auto;
-	}
 	h1,
 	h2 {
 		font-size: 10vw;
 		line-height: 8vw;
+		margin: 0;
 	}
 	@media (max-width: 768px) {
 		.logo {
@@ -36,4 +30,9 @@
 			line-height: 8vh;
 		}
 	}
+	.logo {
+		width: 15rem;
+		height: auto;
+	}
+	
 </style>
