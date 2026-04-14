10699d19501aa9104007d4b779f0f55d9d71619a
Dkrenon <bgsbonilla@gmail.com>
2026-04-14 18:49:18 +0100

    chore: added images, gif and trailer

--- src/routes/trailer/+page.svelte
+++ src/routes/trailer/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { type Content, snippets } from '$lib/components/snippets.svelte';
 	const media: Content[] = [
-		{ id: '1dr6scflI3tWOCqbRp6j0tM5ks-L8IMcs', heading: 'ORUG Walkthrough' }
+		{ id: '1cn3s5SgAvqPk17I1Y3lrcCbHaAUJ31A1', heading: 'ORuggin Trail Trailer' },
+		{ id: '1dr6scflI3tWOCqbRp6j0tM5ks-L8IMcs', heading: 'ORuggin Trail Walkthrough' },
+		{ id: '1A_D8UEAjTBM6Xssoa8ONTN9ytqvpAnqo', heading: 'ORuggin Trail Showcase' }
 	];
 	// { heading: '', description: '', id: '1W_ZyJPR1uU_c30gzGCFI0WCOT757GW_N' }
 </script>
