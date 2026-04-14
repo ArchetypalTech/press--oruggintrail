c30c039b6dd46aebd6633023beaf7b63f8832277
Dkrenon <bgsbonilla@gmail.com>
2026-04-14 12:30:52 +0100

    test: adding O'Ruggin Logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,11 @@
+<script>
+	import logoUrl from '../../icons/logo.svg';
+</script>
+
 <section class="flex min-h-[80vh] flex-col items-center justify-center px-4">
+	<div class="flex flex-col items-center">
+		<img src={logoUrl} alt="O'RUGGIN logo" class="logo" />
+	</div>
 	<div class="flex flex-col items-end justify-center gap-2">
 		<h1 class="font-bold">O'Ruggin Trail</h1>
 		<h2 class="bg-black text-right font-bold text-white">PRESSKIT</h2>
@@ -14,10 +21,18 @@
 		margin: 0;
 	}
 	@media (max-width: 768px) {
+		.logo {
+			width: 10rem;
+		}
 		h1,
 		h2 {
 			font-size: 10vh;
 			line-height: 8vh;
 		}
 	}
+	.logo {
+		width: 15rem;
+		height: auto;
+	}
+	
 </style>
